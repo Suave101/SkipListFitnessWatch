a834149410b5dff7eac474bfd3472deb7ae6f21b
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2026-03-12 14:27:07 -0400

    Add given files

---
+++ src/HW5.java
@@ -0,0 +1,24 @@
+/*
+
+  Author:
+  Email:
+  Course:
+  Section:
+  Description of this file:
+
+ */
+
+public class HW5
+{
+
+    /*
+      Description of each method, including parameters 
+    */
+    public static void main(String[] args)
+    {
+	/* description of variables */
+
+	/* description of each block (around 5-10 lines) of instructions */
+    }
+
+}
