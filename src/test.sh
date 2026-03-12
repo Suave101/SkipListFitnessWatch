16dff76ea8d00e5ed19c12531e53b03ef7837a62
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2026-03-12 14:29:02 -0400

    Add testing script

---
+++ src/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 0. Pull from GitHub for most recent version
+git pull
+echo ""
+
+# 1. Compile all Java files
+
+echo "Compiling..."
+javac *.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed!"
+    exit 1
+fi
+
+# 2. Run tests
+for i in 1 2; do
+    echo "---------------------------"
+    echo "Running Test Case $i..."
+    echo "---------------------------"
+
+    java HW5 hw5in${i}.txt > my_output$i.txt
+
+    # Compare the output with the expected output
+    diff my_output$i.txt hw5out$i.txt > diff_result.txt
+
+    if [ $? -eq 0 ]; then
+        echo "TEST $i: PASSED! ✅"
+    else
+        echo "TEST $i: FAILED! ❌"
+        echo "Differences found between my_output$i.txt and hw5_out$i.txt:"
+        cat diff_result.txt
+    fi
+    echo "---------------------------"
+    echo ""
+done
