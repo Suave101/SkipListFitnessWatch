79d3fb90d2889be1997a9572ba04d97ee5c6c5e4
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2026-03-30 09:30:42 -0400

    Fix for test

--- src/HW5.java
+++ src/HW5.java
@@ -1,50 +1,29 @@
-/*
-
-  Author: Alexander Prescott Doyle
-  Email: adoyle2025@my.fit.edu
-  Course: Data Structures and Algorithms
-  Section: 2
-  Description of this file: A fitness watch program that tracks activities over the course of time
-                            efficiently using skip lists.
-
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class HW5
-{
-
-    /*
-      Description of each method, including parameters 
-    */
+public class HW5 {
     public static void main(String[] args) throws FileNotFoundException {
-        parseAndHandleCommands(args[0]);
+        if (args.length > 0) parseAndHandleCommands(args[0]);
     }
 
     public static void parseAndHandleCommands(String fileName) throws FileNotFoundException {
-        // The input file object that allows for access of the input file
         File inputFile = new File(fileName);
-
-        // Skip List to store all the items in
         SkipListMap skipList = new SkipListMap();
+        FakeRandHeight randHeight = new FakeRandHeight(); // One generator to rule them all
 
-        // Try for safety
         try (Scanner fileScanner = new Scanner(inputFile)) {
-            // Loop through each line of the file after the initial line
             while (fileScanner.hasNextLine()) {
-                // Get the next line in the file
                 String line = fileScanner.nextLine();
-
-                // Split the string into its command and args
                 String[] commandAndArgs = line.split(" ");
+                if (commandAndArgs.length == 0) continue;
 
-                // Determine which method to run and run it
                 switch (commandAndArgs[0]) {
                     case "AddActivity":
-                        addActivity(commandAndArgs[1], commandAndArgs[2], skipList);
+                        // Get the height here to keep the sequence in sync
+                        int h = randHeight.get();
+                        addActivity(commandAndArgs[1], commandAndArgs[2], h, skipList);
                         break;
                     case "RemoveActivity":
                         removeActivity(commandAndArgs[1], skipList);
@@ -64,20 +43,14 @@ public class HW5
                     case "PrintSkipList":
                         printSkipList(skipList);
                         break;
-                    default:
-                        throw new UnsupportedOperationException("The data is formatted wrong and the command " + commandAndArgs[0] + " does not exist!");
                 }
             }
         }
     }
 
-
-    /*
-     * Method that adds an activity by time and activity
-     */
-    public static void addActivity(String time, String activity, SkipListMap skipListMap) {
-        if (!skipListMap.put(time, activity)) {
-            System.out.println("AddActivity " + time + " " + activity + " ExistingActivityError:"+ skipListMap.get(time));
+    public static void addActivity(String time, String activity, int height, SkipListMap skipListMap) {
+        if (!skipListMap.put(time, activity, height)) {
+            System.out.println("AddActivity " + time + " " + activity + " ExistingActivityError:" + skipListMap.get(time));
         } else {
             System.out.println("AddActivity " + time + " " + activity);
         }
@@ -88,11 +61,8 @@ public class HW5
      */
     public static void removeActivity(String time, SkipListMap skipListMap) {
         String removedActivity = skipListMap.remove(time);
-        if (removedActivity == null) {
-            System.out.println("RemoveActivity " + time + " NoActivityError");
-        } else {
-            System.out.println("RemoveActivity " + time + " " + removedActivity);
-        }
+        if (removedActivity == null) System.out.println("RemoveActivity " + time + " NoActivityError");
+        else System.out.println("RemoveActivity " + time + " " + removedActivity);
     }
 
     /*
@@ -100,22 +70,19 @@ public class HW5
      */
     public static void getActivity(String time, SkipListMap skipListMap) {
         String gottenActivity = skipListMap.get(time);
-        if (gottenActivity == null) {
-            System.out.println("GetActivity " + time + " none");
-        } else {
-            System.out.println("GetActivity " + time + " " + gottenActivity);
-        }
+        if (gottenActivity == null) System.out.println("GetActivity " + time + " none");
+        else System.out.println("GetActivity " + time + " " + gottenActivity);
     }
 
     /*
      * Method that gets the activities between the two given times (inclusive)
      */
     public static void getActivitiesBetweenTimes(String startTime, String endTime, SkipListMap skipListMap) {
-        String subMapReturn = skipListMap.subMap(startTime, endTime);
-        if (Objects.equals(subMapReturn, "")) {
+        String subMapReturn = skipListMap.subMap(startTime, endTime).trim();
+        if (subMapReturn.isEmpty()) {
             System.out.println("GetActivitiesBetweenTimes " + startTime + " " + endTime + " none");
         } else {
-            System.out.println("GetActivitiesBetweenTimes " + startTime + " " + endTime + subMapReturn);
+            System.out.println("GetActivitiesBetweenTimes " + startTime + " " + endTime + " " + subMapReturn);
         }
     }
 
@@ -123,13 +90,11 @@ public class HW5
      * Method that gets the activities for a specific day
      */
     public static void getActivitiesForOneDay(String date, SkipListMap skipListMap) {
-        String startTime = date + "0000";
-        String endTime = date + "2359";
-        String subMapReturn = skipListMap.subMap(startTime, endTime);
-        if (Objects.equals(subMapReturn, "")) {
+        String subMapReturn = skipListMap.subMap(date + "0000", date + "2359").trim();
+        if (subMapReturn.isEmpty()) {
             System.out.println("GetActivitiesForOneDay " + date + " none");
         } else {
-            System.out.println("GetActivitiesForOneDay " + date + subMapReturn);
+            System.out.println("GetActivitiesForOneDay " + date + " " + subMapReturn);
         }
     }
 
@@ -138,12 +103,11 @@ public class HW5
      */
     public static void getActivitiesFromEarlierInTheDay(String currentTime, SkipListMap skipListMap) {
         String date = currentTime.substring(0, 4);
-        String startTime = date + "0000";
-        String subMapReturn = skipListMap.subMap(startTime, currentTime);
+        String subMapReturn = skipListMap.subMap(date + "0000", currentTime).trim();
         if (subMapReturn.isEmpty()) {
             System.out.println("GetActivitiesFromEarlierInTheDay " + currentTime + " none");
         } else {
-            System.out.println("GetActivitiesFromEarlierInTheDay " + currentTime + " " + subMapReturn.trim());
+            System.out.println("GetActivitiesFromEarlierInTheDay " + currentTime + " " + subMapReturn);
         }
     }
 
--- src/SkipListMap.java
+++ src/SkipListMap.java
@@ -1,254 +1,131 @@
-/*
-
-  Author: Alexander Prescott Doyle
-  Email: adoyle2025@my.fit.edu
-  Course: Data Structures and Algorithms
-  Section: 2
-  Description of this file: A skip list class to use for the fitness watch tracking software in HW5.java
-
- */
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class SkipListMap {
-
-    // SkipListNode Array
-    private ArrayList<SkipListNode> skipListMap = new ArrayList<SkipListNode>();
-
-    // Random height generator
-    private FakeRandHeight random = new FakeRandHeight();
-
-    // Level of the top empty layer 0-indexed
+    private ArrayList<SkipListNode> skipListMap = new ArrayList<>();
     private int topHeight = 0;
 
-    // Constructor method for SkipList
     public SkipListMap() {
-        // Add first layer to skip list map
-        SkipListNode beginning = new SkipListNode("Beginning", null, null, null, null, null);
-        SkipListNode end = new SkipListNode("End", null, null, beginning, null, null);
+        addLayer(); // Adds S0 with sentinels
+    }
+
+    private void addLayer() {
+        SkipListNode bBelow = skipListMap.isEmpty() ? null : skipListMap.get(skipListMap.size() - 1);
+        SkipListNode eBelow = bBelow == null ? null : getEndSentinel(bBelow);
+
+        SkipListNode beginning = new SkipListNode("Beginning", null, null, null, null, bBelow);
+        SkipListNode end = new SkipListNode("End", null, null, beginning, null, eBelow);
         beginning.setNext(end);
+
+        if (bBelow != null) bBelow.setAbove(beginning);
+        if (eBelow != null) eBelow.setAbove(end);
+
         skipListMap.add(beginning);
     }
 
-    // Method to print the array
-    public void print() {
-        int curLayer = skipListMap.size() - 2;
-        System.out.println("(S" + (curLayer + 1) + ") empty");
-        // Iterate backwards through the arraylist so top layer prints first
-        for (int i = skipListMap.size() - 2; i >= 0; i--) {
-            System.out.print("(S" + curLayer + ")");
-            SkipListNode cur = skipListMap.get(i).getNext();
-            while (!cur.getTime().equals("End")) {
-                System.out.print(" " + cur.getTime() + ":" + cur.getActivity());
-                cur = cur.getNext();
-            }
-            System.out.println();
-            curLayer--;
-        }
+    private SkipListNode getEndSentinel(SkipListNode node) {
+        while (!node.getTime().equals("End")) node = node.getNext();
+        return node;
     }
 
-    // Method to add an item to the SkipListMap. Returns false if time already exists
-    public boolean put(String time, String activity) {
-        // Get Random Height
-        int height = random.get();
-
-        // Parse String -> Integer for time
+    public boolean put(String time, String activity, int height) {
         int t = Integer.parseInt(time);
+        ArrayList<SkipListNode> path = new ArrayList<>();
 
-        // Find the node where this time should go
-        SkipListNode nodeBefore = findNode(t);
+        // Ensure layers exist
+        while (skipListMap.size() <= height + 1) addLayer();
 
-        // Ensure that the element does not exist already
-        if (nodeBefore != null && nodeBefore.getIntTime() == t) {
-            return false;
+        // Search and record path
+        SkipListNode cur = skipListMap.get(skipListMap.size() - 1);
+        while (cur != null) {
+            while (cur.getNext() != null && cur.getNext().getIntTime() < t) {
+                cur = cur.getNext();
+            }
+            path.add(0, cur); // index 0 is S0, index 1 is S1...
+            cur = cur.getBelow();
         }
 
-        // Get the node after where the new node will be
-        SkipListNode nodeAfter = nodeBefore.getNext();
+        // Duplicate check
+        SkipListNode possibleMatch = path.get(0).getNext();
+        if (possibleMatch != null && possibleMatch.getIntTime() == t) return false;
 
-        // Insert at bottom level between nodeBefore and nodeAfter
-        SkipListNode newNode = new SkipListNode(time, activity, nodeAfter, nodeBefore, null, null);
-        nodeBefore.setNext(newNode);
-        nodeAfter.setPrev(newNode);
-
-        // Ensure there is always an empty layer at top
-        ensureMaxHeight(height);
-
-        // Build tower upwards
-        SkipListNode belowRef = newNode;
-        while (height > 0) {
-            // move left until we find a node that has an above pointer
-            SkipListNode left = nodeBefore;
-            while (left != null && left.getAbove() == null) {
-                left = left.getPrev();
-            }
-
-            // left should never be null because the "Beginning" sentinel has above pointers
-            SkipListNode leftAbove = left.getAbove();
-            SkipListNode rightAbove = leftAbove.getNext();
-
-            SkipListNode newAbove = new SkipListNode(time, activity, rightAbove, leftAbove, null, belowRef);
-
-            // Link horizontally
-            leftAbove.setNext(newAbove);
-            rightAbove.setPrev(newAbove);
-
-            // Link vertically
-            belowRef.setAbove(newAbove);
-
-            // Move up
-            belowRef = newAbove;
-            nodeBefore = leftAbove;
-            height--;
+        // Build tower
+        SkipListNode belowNode = null;
+        for (int i = 0; i <= height; i++) {
+            SkipListNode left = path.get(i);
+            SkipListNode newNode = new SkipListNode(time, activity, left.getNext(), left, null, belowNode);
+            left.getNext().setPrev(newNode);
+            left.setNext(newNode);
+            if (belowNode != null) belowNode.setAbove(newNode);
+            belowNode = newNode;
         }
         return true;
     }
 
-    // Method to get an item by key from the SkipListMap
-    public String get(String time) {
-        int key = Integer.parseInt(time);
-
-        // Find node iteratively
-        SkipListNode node = findNode(key);
-
-        // Check if node was found
-        if (node != null && node.getIntTime() == key) {
-            return node.getActivity();
-        } else {
-            return null;
+    public void print() {
+        // Standard Skip List printing: Top layer to bottom
+        for (int i = skipListMap.size() - 1; i >= 0; i--) {
+            System.out.print("(S" + i + ")");
+            SkipListNode cur = skipListMap.get(i).getNext();
+            if (cur.getTime().equals("End")) {
+                System.out.println(" empty");
+            } else {
+                while (!cur.getTime().equals("End")) {
+                    System.out.print(" " + cur.getTime() + ":" + cur.getActivity());
+                    cur = cur.getNext();
+                }
+                System.out.println();
+            }
         }
     }
 
-    // Method to remove an item by key from the SkipListMap
-    public String remove(String time) {
-        int key = Integer.parseInt(time);
-
-        // Get the bottom node of the stack we are looking for
-        SkipListNode bottomNode = findNode(key);
-
-        // Check if the node exists
-        if (bottomNode == null || bottomNode.getIntTime() != key) {
-            return null;
-        }
-
-        String activity = bottomNode.getActivity();
-
-        // Recursively remove the tower from the bottom up
-        collapseTower(bottomNode);
-
-        // Return the activity we deleted
-        return activity;
-    }
-
-    // Method to subMap the map
-    public String subMap(String startTime, String endTime) {
-        StringBuilder sb = new StringBuilder();
-        int start = Integer.parseInt(startTime);
-        int end = Integer.parseInt(endTime);
-
-        // findNode gracefully finds the node <= startTime
-        SkipListNode cur = findNode(start);
-
-        // If the exact start time doesn't exist, shift forward to the next available time
-        if (cur != null && cur.getIntTime() < start) {
-            cur = cur.getNext();
-        }
-
-        // Keep going till end of our range
-        while (cur != null && !cur.getTime().equals("End") && cur.getIntTime() <= end) {
-            sb.append(" ").append(cur.getTime()).append(":").append(cur.getActivity());
-            cur = cur.getNext();
-        }
-
-        return sb.toString();
-    }
-
-    // Helper method to remove a tower recursively from bottom to top
-    private void collapseTower(SkipListNode node) {
-        // Get nodes neighbors
-        SkipListNode leftOfNode = node.getPrev();
-        SkipListNode rightOfNode = node.getNext();
-        SkipListNode topOfNode = node.getAbove();
-
-        // Make horizontal neighbors forget node exists
-        leftOfNode.setNext(rightOfNode);
-        rightOfNode.setPrev(leftOfNode);
-
-        // If top neighbor exists, recurse upwards
-        if (topOfNode != null) {
-            collapseTower(topOfNode);
-        }
-    }
-
+    // ... [get, remove, and subMap remain largely as you had them, utilizing findNode] ...
     private SkipListNode findNode(int time) {
         SkipListNode cur = skipListMap.get(skipListMap.size() - 1);
         while (cur != null) {
-            // Look ahead
-            if (cur.getNext() != null && cur.getNext().getIntTime() <= time) {
+            while (cur.getNext() != null && cur.getNext().getIntTime() <= time) {
                 cur = cur.getNext();
-            } else {
-                // Otherwise drop down
-                if (cur.getBelow() == null) {
-                    return cur; // Bottom reached
-                }
-                cur = cur.getBelow();
             }
+            if (cur.getBelow() == null) return cur;
+            cur = cur.getBelow();
         }
         return cur;
     }
 
-    // Given the height of a node, ensure that there is an empty layer above its max height
-    private void ensureMaxHeight(int height) {
-        while (this.topHeight <= height) {
-            addLayer();
+    public String get(String time) {
+        SkipListNode node = findNode(Integer.parseInt(time));
+        return (node != null && node.getTime().equals(time)) ? node.getActivity() : null;
+    }
+
+    public String remove(String time) {
+        SkipListNode node = findNode(Integer.parseInt(time));
+        if (node == null || !node.getTime().equals(time)) return null;
+        String act = node.getActivity();
+        while (node != null) {
+            node.getPrev().setNext(node.getNext());
+            node.getNext().setPrev(node.getPrev());
+            node = node.getAbove();
         }
+        return act;
     }
 
-    // Method to add a layer to the SkipListMap
-    private void addLayer() {
-        // Get the beginning of the current top layer
-        SkipListNode beginningBelow = skipListMap.get(skipListMap.size() - 1);
-
-        // Get the end of the current top layer
-        SkipListNode endBelow = findValueHoriz(beginningBelow, "End");
-
-        // Add layer to skip list map
-        SkipListNode beginning = new SkipListNode("Beginning", null, null, null, null, beginningBelow);
-        SkipListNode end = new SkipListNode("End", null, null, beginning, null, endBelow);
-        beginning.setNext(end);
-        skipListMap.add(beginning);
-        beginningBelow.setAbove(beginning);
-        endBelow.setAbove(end);
-        // Increment the top height
-        topHeight++;
-    }
-
-    // A method to recurse through list left to right to find item with str val
-    private SkipListNode findValueHoriz(SkipListNode beginning, String value) {
-        if (Objects.equals(beginning.getTime(), value)) {
-            return beginning;
-        } else if (Objects.equals(beginning.getTime(), "End")) {
-            return null;
-        } else {
-            return findValueHoriz(beginning.getNext(), value);
+    public String subMap(String startTime, String endTime) {
+        StringBuilder sb = new StringBuilder();
+        int start = Integer.parseInt(startTime);
+        int end = Integer.parseInt(endTime);
+        SkipListNode cur = findNode(start);
+        if (cur.getIntTime() < start) cur = cur.getNext();
+        while (cur != null && cur.getIntTime() <= end) {
+            sb.append(" ").append(cur.getTime()).append(":").append(cur.getActivity());
+            cur = cur.getNext();
         }
+        return sb.toString();
     }
 
-    // A SkipList Node
+    // ... [Your SkipListNode inner class remains the same] ...
     private class SkipListNode {
-        // The nodes around the node
-        private SkipListNode next;
-        private SkipListNode prev;
-        private SkipListNode above;
-        private SkipListNode below;
-
-        // Key
-        private String time;
+        private SkipListNode next, prev, above, below;
+        private String time, activity;
         private int intTime;
-
-        // Value
-        private String activity;
 
         // Constructor method for SkipListNode
         public SkipListNode(String time, String activity, SkipListNode next, SkipListNode prev, SkipListNode above, SkipListNode below) {
