3e7bea51f27f700b160515f4b0ea4c42f5571274
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2026-03-31 14:38:15 -0400

    Add test case

--- src/test.sh
+++ src/test.sh
@@ -15,7 +15,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # 2. Run tests
-for i in 1 2 3 4 5; do
+for i in 1 2 3 4 5 6; do
     echo "---------------------------"
     echo "Running Test Case $i..."
     echo "---------------------------"
